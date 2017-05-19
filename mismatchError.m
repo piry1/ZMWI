a6451aabefe4847041caa1bcf896a73d3255dcab
Piotr Konieczny <Piotr Konieczny>
2017-05-19 16:49:59 +0200

    liczenie błędu dopasowania

---
+++ mismatchError.m
@@ -0,0 +1,15 @@
+function [ error ] = mismatchError( results, labels )
+
+len = length(labels);
+errCount = 0;
+
+for i=1:len
+    if(results(i) ~= labels(i))
+        errCount = errCount +1;
+    end
+end
+
+    error = errCount / len;
+    
+end
+
